592631343f19647492ee52cce845a5af27247695
11100010 <87060114+11100010@users.noreply.github.com>
2024-04-19 09:51:23 +0700

    Update utils.py

--- WebChatGPT/utils.py
+++ WebChatGPT/utils.py
@@ -26,7 +26,7 @@ headers = request_headers = {
     "Sec-Fetch-Dest": "empty",
     "Sec-Fetch-Mode": "cors",
     "Sec-Fetch-Site": "same-origin",
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
     "OAI-Language": locale.getlocale()[0].replace("_", "-"),
 }
 
